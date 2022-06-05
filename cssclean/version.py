116c72b6ce158748c6bda87961e166155b99e158
vsoch <vsoch@users.noreply.github.com>
2022-06-05 12:53:25 -0600

    updating dependency tinycss -> tinycss2 and adding .all-conributors file
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- cssclean/version.py
+++ cssclean/version.py
@@ -2,7 +2,7 @@ __author__ = "Vanessa Sochat"
 __copyright__ = "Copyright 2022, Vanessa Sochat"
 __license__ = "MPL 2.0"
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 AUTHOR = "Vanessa Sochat"
 EMAIL = "vsoch@users.noreply.github.com"
 NAME = "cssclean"
@@ -15,7 +15,7 @@ LICENSE = "LICENSE"
 # Global requirements
 
 INSTALL_REQUIRES = (
-    ("tinycss", {"min_version": None}),
+    ("tinycss2", {"min_version": None}),
     ("beautifulsoup4", {"min_version": None}),
 )
 
