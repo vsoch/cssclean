e04cb3f8882dd8c3dbc1ebae8144097baca3b180
vsoch <vsoch@users.noreply.github.com>
2022-06-06 17:58:35 -0600

    black
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- cssclean/utils/__init__.py
+++ cssclean/utils/__init__.py
@@ -1,9 +1,29 @@
 from .docs import get_docstring
-from .fileio import (copyfile, creation_date, get_file_hash, get_tmpdir,
-                     get_tmpfile, list_modules, mkdir_p, mkdirp, print_json,
-                     read_file, read_json, recursive_find, workdir, write_file,
-                     write_json)
+from .fileio import (
+    copyfile,
+    creation_date,
+    get_file_hash,
+    get_tmpdir,
+    get_tmpfile,
+    list_modules,
+    mkdir_p,
+    mkdirp,
+    print_json,
+    read_file,
+    read_json,
+    recursive_find,
+    workdir,
+    write_file,
+    write_json,
+)
 from .inspect import dynamic_import
-from .terminal import (check_install, confirm_action, confirm_uninstall,
-                       get_installdir, get_user, get_userhome, run_command,
-                       which)
+from .terminal import (
+    check_install,
+    confirm_action,
+    confirm_uninstall,
+    get_installdir,
+    get_user,
+    get_userhome,
+    run_command,
+    which,
+)
